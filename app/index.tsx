98f2e7bff05c085dc9d8042ae5d0f2cbe8b089cb
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-02-11 02:13:27 +0600

    tracker for project tutorial

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-Link
 
 export default function App() {
   return (
