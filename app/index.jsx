3a543e3be514025bf553d27305d5804fd73cd656
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-02-17 00:59:46 +0600

    Creating the Onboarding

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,43 @@
 import { Link } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../constant'
 
 export default function App () {
   return (
-    <View className='flex-1 justify-center items-center bg-white'>
-      <Text className='text-3xl font-pblack'>Aura!</Text>
-      <StatusBar style='auto' />
-      <Link href='/home' style={{ color: 'blue' }}>
-        Home
-      </Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full h-full items-center px-4'>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-white text-3xl text-center font-bold'>
+              Discover Endless {'\n'} Possibilities with{' '}
+              <Text className='text-secondary-200'>Aura</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className='w-[130px] h-[15px] absolute -bottom-[10px] -right-8'
+              resizeMode='contain'
+            />
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 text-center mt-7'>
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aura
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
