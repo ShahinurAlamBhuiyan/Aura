4e93078cb984c1a475a4b3c9504ba9ef378db860
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-02-23 00:05:02 +0600

    implementing auth design and fixing some bug

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,7 +1,7 @@
 import { View, Text, Image } from 'react-native'
 import { Tabs, Redirect } from 'expo-router'
 
-import { icons } from '../../constant'
+import { icons } from '../../constants'
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Image, ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../constant'
+import { images } from '../constants'
 import CustomButton from '../components/CustomButton'
 import { StatusBar } from 'expo-status-bar'
 import { router } from 'expo-router'
