fbb8250a26b90e7ab26d3f76adc28dda36301e3a
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-05 01:20:39 +0600

    add loggedin user to the context

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -7,7 +7,9 @@ import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link, router } from 'expo-router'
 import { signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext()
   const [isSubmitting, setIsSubmitted] = useState(false)
   const [form, setForm] = useState({
     email: '',
@@ -23,6 +25,10 @@ const SignIn = () => {
     try {
       const result = await signIn(form.email, form.password)
       // set it to global state...
+      setUser(result)
+      setIsLoggedIn(true)
+
+      Alert.alert('Success', 'User has been signed in successfully!')
       router.replace('/home')
     } catch (error) {
       Alert.alert('Error', error.message)
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -7,7 +7,9 @@ import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext()
   const [isSubmitting, setIsSubmitted] = useState(false)
   const [form, setForm] = useState({
     username: '',
@@ -16,14 +18,17 @@ const SignUp = () => {
   })
 
   const submit = async () => {
-    if (!form.username || !form.password || !form.email) {
+    if (form.username === '' || form.password === '' || form.email === '') {
       Alert.alert('Error', 'Please fill in all the fields!')
     }
     setIsSubmitted(true)
 
     try {
       const result = await createUser(form.email, form.password, form.username)
-      // set it to global state...
+      // set to global state
+      setUser(result)
+      setIsLoggedIn(true)
+
       router.replace('/home')
     } catch (error) {
       Alert.alert('Error', error.message)
