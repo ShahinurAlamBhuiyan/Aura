1916ef512d79be8858667607be0c1ded965443a4
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-03-09 23:21:14 +0600

    Start working with home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Text className='text-3xl'>{item.id}</Text>}
+      />
+    </SafeAreaView>
   )
 }
 export default Home
