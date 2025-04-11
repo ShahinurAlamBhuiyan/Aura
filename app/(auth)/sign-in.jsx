ad2ee11282ccfbbe737cc63aa10e7e1e681b6419
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-04-11 18:37:40 +0600

    debug global context

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -6,7 +6,7 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link, router } from 'expo-router'
-import { createUser, signIn } from '../../lib/appwrite'
+import { createUser, getCurrentUser, signIn } from '../../lib/appwrite'
 import { useGlobalContext } from '../../context/GlobalProvider'
 const SignIn = () => {
   const { setUser, setIsLoggedIn } = useGlobalContext()
@@ -23,8 +23,8 @@ const SignIn = () => {
     setIsSubmitted(true)
 
     try {
-      const result = await signIn(form.email, form.password)
-      // set it to global state...
+      await signIn(form.email, form.password)
+      const result = await getCurrentUser()
       setUser(result)
       setIsLoggedIn(true)
 
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,7 +8,10 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
+
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext()
   const { data: posts, refetchData } = useAppwrite(getAllPosts)
   const { data: latestPosts } = useAppwrite(getLatestPosts)
 
@@ -20,7 +23,7 @@ const Home = () => {
     await refetchData()
     setRefreshing(false)
   }
-
+  console.log(user)
   return (
     <SafeAreaView className='bg-primary h-full'>
       <FlatList
@@ -32,10 +35,10 @@ const Home = () => {
             <View className='justify-between items-start flex-row mb-6'>
               <View>
                 <Text className='font-pmedium text-sm text-gray-100'>
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className='text-2xl font-psemibold text-white'>
-                  Shahinur Alam Bhuiyan
+                  {user?.username}
                 </Text>
               </View>
 
